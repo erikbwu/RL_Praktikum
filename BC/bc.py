8d0069ae3568ebc7c04ad857a66402ac02f9b9a4
Erik Wu <erik.wu@gmx.de>
2024-01-29 08:03:12 +0100

    working project :)

--- BC/bc.py
+++ BC/bc.py
@@ -11,7 +11,7 @@ from sofa_env.base import RenderMode
 from sofa_env.scenes.ligating_loop.ligating_loop_env import LigatingLoopEnv, ObservationType, ActionType
 from gymnasium.wrappers import TimeLimit
 from sofa_env.wrappers.point_cloud import PointCloudFromDepthImageObservationWrapper
-from stable_baselines3.common.evaluation import evaluate_policy
+# from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
@@ -21,6 +21,120 @@ from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from torch_geometric.data import Data, Batch
 from torch_geometric.nn import PointNetConv, fps, radius, global_max_pool, MLP
+
+
+def evaluate_policy(
+    model,
+    env,
+    n_eval_episodes: int = 10,
+    deterministic: bool = True,
+    render: bool = False,
+    callback = None,
+    reward_threshold = None,
+    return_episode_rewards: bool = False,
+    warn: bool = True,
+):
+    """
+    Runs policy for ``n_eval_episodes`` episodes and returns average reward.
+    If a vector env is passed in, this divides the episodes to evaluate onto the
+    different elements of the vector env. This static division of work is done to
+    remove bias. See https://github.com/DLR-RM/stable-baselines3/issues/402 for more
+    details and discussion.
+
+    .. note::oki
+        If environment has not been wrapped with ``Monitor`` wrapper, reward and
+        episode lengths are counted as it appears with ``env.step`` calls. If
+        the environment contains wrappers that modify rewards or episode lengths
+        (e.g. reward scaling, early episode reset), these will affect the evaluation
+        results as well. You can avoid this by wrapping environment with ``Monitor``
+        wrapper before anything else.
+
+    :param model: The RL agent you want to evaluate. This can be any object
+        that implements a `predict` method, such as an RL algorithm (``BaseAlgorithm``)
+        or policy (``BasePolicy``).
+    :param env: The gym environment or ``VecEnv`` environment.
+    :param n_eval_episodes: Number of episode to evaluate the agent
+    :param deterministic: Whether to use deterministic or stochastic actions
+    :param render: Whether to render the environment or not
+    :param callback: callback function to do additional checks,
+        called after each step. Gets locals() and globals() passed as parameters.
+    :param reward_threshold: Minimum expected reward per episode,
+        this will raise an error if the performance is not met
+    :param return_episode_rewards: If True, a list of rewards and episode lengths
+        per episode will be returned instead of the mean.
+    :param warn: If True (default), warns user about lack of a Monitor wrapper in the
+        evaluation environment.
+    :return: Mean reward per episode, std of reward per episode.
+        Returns ([float], [int]) when ``return_episode_rewards`` is True, first
+        list containing per-episode rewards and second containing per-episode lengths
+        (in number of steps).
+    """
+    is_monitor_wrapped = False
+
+    n_envs = 1
+    episode_rewards = []
+    episode_lengths = []
+
+    episode_counts = np.zeros(n_envs, dtype="int")
+    # Divides episodes among different sub environments in the vector as evenly as possible
+    episode_count_targets = np.array([(n_eval_episodes + i) // n_envs for i in range(n_envs)], dtype="int")
+
+    current_rewards = np.zeros(n_envs)
+    current_lengths = np.zeros(n_envs, dtype="int")
+    observations, infos = env.reset()
+    states = None
+    episode_starts = np.ones((1,), dtype=bool)
+
+    while (episode_counts < episode_count_targets).any():
+        actions, _ = model.predict(
+            observations
+        )
+        new_observations, rewards, dones, truncated, infos = env.step(actions)
+        current_rewards += rewards
+        current_lengths += 1
+        for i in range(n_envs):
+            if episode_counts[i] < episode_count_targets[i]:
+                # unpack values so that the callback can access the local variables
+                reward = rewards
+                done = dones
+                info = infos
+                episode_starts = done
+
+                if callback is not None:
+                    callback(locals(), globals())
+
+                if dones:
+                    if is_monitor_wrapped:
+                        # Atari wrapper can send a "done" signal when
+                        # the agent loses a life, but it does not correspond
+                        # to the true end of episode
+                        if "episode" in info.keys():
+                            # Do not trust "done" with episode endings.
+                            # Monitor wrapper includes "episode" key in info if environment
+                            # has been wrapped with it. Use those rewards instead.
+                            episode_rewards.append(info["episode"]["r"])
+                            episode_lengths.append(info["episode"]["l"])
+                            # Only increment at the real end of an episode
+                            episode_counts[i] += 1
+                    else:
+                        episode_rewards.append(current_rewards[i])
+                        episode_lengths.append(current_lengths[i])
+                        episode_counts[i] += 1
+                    current_rewards[i] = 0
+                    current_lengths[i] = 0
+
+        observations = new_observations
+
+        if render:
+            env.render()
+
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+    if reward_threshold is not None:
+        assert mean_reward > reward_threshold, "Mean reward below threshold: " f"{mean_reward:.2f} < {reward_threshold:.2f}"
+    if return_episode_rewards:
+        return episode_rewards, episode_lengths
+    return mean_reward, std_reward
 
 
 class SAModule(torch.nn.Module):
@@ -71,24 +185,22 @@ class PointNetFeaturesExtractor(BaseFeaturesExtractor):
 
         self.mlp = MLP([1024, 512, features_dim], norm=None)
 
-    def forward(self, observations: Batch) -> torch.Tensor:
-        # num_points = torch.full((observations.shape[0],), 65536)
-        # batch = torch.repeat_interleave(
-        #     torch.arange(len(num_points), device=num_points.device),
-        #     repeats=num_points,
-        # )
-        # flattened_observations = torch.flatten(observations, end_dim=1)
-        #
-        # data = Data(pos=flattened_observations, batch=batch)
-
+    def forward(self, observations: Data) -> torch.Tensor:
+        if isinstance(observations, torch.Tensor):
+            # num_points = torch.full((observations.shape[0],), 1)
+            # batch = torch.repeat_interleave(
+            #     torch.arange(len(num_points), device=num_points.device),
+            #     repeats=num_points,
+            # )
+            # flattened_observations = torch.flatten(observations, end_dim=1)
+            observations = Data(pos=observations, batch=torch.full((len(observations),), 0)).to(observations.device)
 
         sa0_out = (None, observations.pos.to(torch.float32), observations.batch)
-       # print(data.pos.shape)
         sa1_out = self.sa1_module(*sa0_out)
         sa2_out = self.sa2_module(*sa1_out)
         sa3_out = self.sa3_module(*sa2_out)
         x, pos, batch = sa3_out
-        return self.mlp(x).log_softmax(dim=-1)
+        return self.mlp(x).log_softmax(dim=-1) # returns 2 stacked vectors
 
 
 
@@ -163,8 +275,11 @@ def make_transitions(n_traj = 10):
         obs.extend(pcds)
         actions.extend(action)
         dones.extend(done)
-    print(np.asarray(actions).shape)
+        print(f'{i + 1}/{n_traj}: actions:{len(actions)}  observations:{len(obs)}')
+
+    print('actionsshape:' + str(np.asarray(actions).shape))
     return Transitions(obs[:-1], np.asarray(actions), np.array([{}] * len(actions)), obs[1:],  np.asarray(dones))
+
 
 
 
@@ -186,10 +301,14 @@ bc_trainer = bc.BC_Pyg(
     policy=policy,
     rng=rng,
     device='cuda',
-    batch_size=2,
+    batch_size=8,
+    minibatch_size=2,
 )
+
+#reward_before_training, _ = evaluate_policy(bc_trainer.policy, _make_env(), 10)
+#print(f"Reward before training: {reward_before_training}")
 bc_trainer.train(n_epochs=1)
-reward_after_training, _ = evaluate_policy(bc_trainer.policy, env, 10)
+reward_after_training, _ = evaluate_policy(bc_trainer.policy, _make_env(), 10)
 print(f"Reward after training: {reward_after_training}")
 
 print('done')
