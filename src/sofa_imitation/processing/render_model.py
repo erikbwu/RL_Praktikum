12b2012146e086067abf0966244aba3e66fbd63c
Erik Wu <erik.wu@understand.ai>
2024-02-10 16:13:01 +0100

    try to fix wandb

--- src/sofa_imitation/processing/render_model.py
+++ src/sofa_imitation/processing/render_model.py
@@ -1,17 +1,17 @@
-from sofa_env.base import RenderMode
+from policy.PointNetActorCritic import PointNetActorCriticPolicy
+from util.env_from_string import get_env
+from util.evaluate_policy import evaluate_policy
+
 
 def render_model(model_path: str, env_name: str):
-    image_shape = (256,256)
+    env = get_env(env_name, should_render=True)
+    policy = PointNetActorCriticPolicy(env.observation_space, env.action_space, lambda _ :1e-4, [256, 128])
+    policy = policy.load(model_path)
+    evaluate_policy(policy, env, 1)
 
-    if env_name == "ligating_loop":
-        from sofa_env.scenes.ligating_loop.ligating_loop_env import LigatingLoopEnv, ObservationType, ActionType
 
-        env = LigatingLoopEnv(
-            observation_type=ObservationType.RGBD,
-            render_mode=RenderMode.HUMAN,
-            action_type=ActionType.CONTINUOUS,
-            image_shape=image_shape,
-            frame_skip=2,
-            time_step=0.1,
-            settle_steps=50,
-        )
+
+if __name__ == "__main__":
+    model_path = '/home/erik/RL_Praktikum/src/sofa_imitation/model/ligating_loop/2024-02-10_14:00/run_4'
+    render_model(model_path, 'ligating_loop')
+
