4924e898cd793e32dbbd6b3c7f717108481f41eb
Erik Wu <erik.wu@gmx.de>
2024-03-15 20:12:53 +0100

    working

--- src/sofa_imitation/policy/PointNetRewardNet.py
+++ src/sofa_imitation/policy/PointNetRewardNet.py
@@ -1,9 +1,13 @@
+import numpy as np
+from torch_geometric.data import Data
+
 from imitation.rewards.reward_nets import RewardNet
 import gymnasium as gym
 from torch_geometric.nn import PointNetConv, fps, radius, global_max_pool, MLP
 from torch_geometric.transforms import GridSampling
+import torch
 
-from sofa_imitation.policy.PointNetActorCritic import SAModule, GlobalSAModule
+from .PointNetActorCritic import SAModule, GlobalSAModule
 
 
 class PointNetRewardNet(RewardNet):
@@ -14,6 +18,7 @@ class PointNetRewardNet(RewardNet):
         action_space: gym.Space,
         grid_size: float,
         inp_features_dim: int,
+        action_features_dim: int,
         **kwargs,
     ):
         """Builds reward MLP.
@@ -33,23 +38,59 @@ class PointNetRewardNet(RewardNet):
         self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
         self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
 
-        self.mlp = MLP([1024, 512, 128], norm=None)
+        self.mlp_points = MLP([1024, 512, 128], norm=None)
+        self.mlp_global = MLP([128 + action_features_dim, 64, 32, 1], norm=None)
         self.grid_sampling = GridSampling(grid_size)  # 3 ~> 1100, 2.5 ~> 1500
 
     def forward(self, state, action, next_state, done):
-        inputs = []
-        if self.use_state:
-            inputs.append(th.flatten(state, 1))
-        if self.use_action:
-            inputs.append(th.flatten(action, 1))
-        if self.use_next_state:
-            inputs.append(th.flatten(next_state, 1))
-        if self.use_done:
-            inputs.append(th.reshape(done, [-1, 1]))
 
-        inputs_concat = th.cat(inputs, dim=1)
+        if isinstance(state, np.ndarray):
+            state = torch.from_numpy(state).float().to(self.device)
+            if len(state.shape) == 3 and state.shape[0] == 1:  # has batch dimension
+                state = state[0]
 
-        outputs = self.mlp(inputs_concat)
-        assert outputs.shape == state.shape[:1]
+            assert len(state.shape) == 2  # only handles one array observation
+            if state.shape[-1] != 3:  # has color
+                state = Data(pos=state[:, :3], batch=torch.full((len(state),), 0),
+                                    x=state[:, 3:]).to(state.device)
+            else:
+                state = Data(pos=state, batch=torch.full((len(state),), 0)).to(
+                    state.device)
+            # display_array(state.pos.cpu(), state.x.cpu())
+        if len(state.pos) > 5000:
+            state = self.grid_sampling(state)
+            # display_array(state.pos.cpu(), state.x.cpu())
+        sa0_out = (state.x.to(torch.float32), state.pos.to(torch.float32), state.batch)
+        sa1_out = self.sa1_module(*sa0_out)
+        sa2_out = self.sa2_module(*sa1_out)
+        sa3_out = self.sa3_module(*sa2_out)
+        x, pos, batch = sa3_out
+
+        features = torch.cat([self.mlp_points(x), torch.tensor(action).to(self.device)], dim=1)
+        #print(features.shape)
+        outputs = self.mlp_global(features)
 
         return outputs
+
+    def predict(
+        self,
+        state,
+        action: np.ndarray,
+        next_state,
+        done,
+    ) -> np.ndarray:
+        """Compute rewards for a batch of transitions without gradients.
+
+        Converting th.Tensor rewards from `predict_th` to NumPy arrays.
+
+        Args:
+            state: Current states of shape `(batch_size,) + state_shape`.
+            action: Actions of shape `(batch_size,) + action_shape`.
+            next_state: Successor states of shape `(batch_size,) + state_shape`.
+            done: End-of-episode (terminal state) indicator of shape `(batch_size,)`.
+
+        Returns:
+            Computed rewards of shape `(batch_size,)`.
+        """
+        rew_th = self(state, action, next_state, done)
+        return rew_th.detach().cpu().numpy().flatten()
