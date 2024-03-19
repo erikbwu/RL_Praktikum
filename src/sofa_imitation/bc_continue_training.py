410cfe925ebd566330b83787b3c32a8a6492e842
Erik Wu <erik.wu@gmx.de>
2024-03-19 18:00:46 +0100

    update

--- src/sofa_imitation/bc_continue_training.py
+++ src/sofa_imitation/bc_continue_training.py
@@ -8,33 +8,38 @@ import numpy as np
 from imitation.algorithms import bc
 
 from policy.PointNetActorCritic import PointNetActorCriticPolicy
-from util.env_from_string import get_env
-from util.data import npz_to_transitions
+from util.env_from_string import get_env, get_grid_size_from_string
+from util.data import npz_to_transitions, npz_to_state_transitions
 from util.evaluate_policy import evaluate_policy
 import wandb
 
 log = logging.getLogger(__name__)
 
 
-def run_bc(batch_size: int = 2, learning_rate=lambda epoch: 1e-3 * 0.99 ** epoch, num_epoch: int = 1,
-           num_traj: int = 5, use_color: bool = False, n_eval: int = 0):
-    n_run = 133
-    start_time = '2024-02-24_01:47'
+def run_bc(env_name, env_prefix, batch_size: int = 2, learning_rate=lambda epoch: 1e-3 * 0.99 ** epoch, num_epoch: int = 1,
+           num_traj: int = 5, use_color: bool = False, use_state = False, n_eval: int = 0):
+    n_run = 28
+    start_time = '2024-03-17_15:53'
     path = '../../../sofa_env_demonstrations/ligating_loop'
-    model_path = f'./model/BC/ligating_loop/{start_time}/run_{n_run}'
-
+    model_path = f'./model/BC/rope_cutting/{start_time}/run_{n_run}'
+    grid_size = get_grid_size_from_string(env_name)
 
     if isinstance(learning_rate, float) or isinstance(learning_rate, int):
         lr = learning_rate
         learning_rate = lambda _: lr
 
-    env = get_env('ligating_loop', False)
+    env = get_env(env_name, use_state, should_render=True)
 
     rng = np.random.default_rng()
-    policy = PointNetActorCriticPolicy(env.observation_space, env.action_space, learning_rate, [256, 128], 1, 3 if use_color else 0)
-    policy = policy.load(model_path, device='cuda')
 
-    demos = npz_to_transitions(path, 'LigatingLoopEnv_', num_traj, use_color, 0.001)
+    if use_state:
+        pass
+
+    else:
+        demos = npz_to_transitions(path, env_prefix, num_traj, use_color, grid_size['Demo'])
+        policy = PointNetActorCriticPolicy(env.observation_space, env.action_space, learning_rate, [256, 128, 64, 32],
+                                           grid_size['FeatureExtractor'], 3 if use_color else 0)
+        policy.load(model_path, device='cuda')
 
     log.info('Finished loading train data')
 
@@ -49,17 +54,17 @@ def run_bc(batch_size: int = 2, learning_rate=lambda epoch: 1e-3 * 0.99 ** epoch
         optimizer_kwargs={'lr': learning_rate(0)}
     )
 
-    Path(f'./model/ligating_loop/{start_time}/').mkdir(parents=True, exist_ok=True)
+    Path(f'./model/BC/{env_name}/{start_time}/').mkdir(parents=True, exist_ok=True)
     while True:
-        n_run += 1
-        bc_trainer.train(n_epochs=num_epoch, progress_bar=True, log_interval=10,)
-        bc_trainer.policy.save(f'./model/BC/ligating_loop/{start_time}/run_{n_run}')
-
+        bc_trainer.train(n_epochs=num_epoch, progress_bar=True, log_interval=50, )
+        bc_trainer.policy.save(f'./model/BC/{env_name}/{start_time}/run_{n_run}')
+        # save_stable_model(Path(f'./model/ligating_loop/{start_time}'), bc_trainer.policy, f'run_{n_run}')
         log.info('Finished run and saved model')
 
         reward_after_training, std_reward = evaluate_policy(bc_trainer.policy, env, n_eval)
         wandb.log({"reward": reward_after_training, "std_reward": std_reward, "epoch": n_run * num_epoch})
         log.info(f"Reward after training run {n_run}: {reward_after_training}")
+        n_run += 1
     log.info('done')
 
 
@@ -74,12 +79,15 @@ def hydra_run(cfg: DictConfig):
     num_traj = cfg.hyperparameter.number_trajectories
     use_color = cfg.hyperparameter.use_color
     n_eval = cfg.hyperparameter.number_evaluations
+    env_name = cfg.env.env_name
+    env_prefix = cfg.env.env_prefix
+    use_state = cfg.hyperparameter.use_state
 
     wandb.init(project="Imitation_Sofa", config=OmegaConf.to_container(cfg, resolve=True),
-               settings=wandb.Settings(start_method="thread"), notes='increased pointcloud size, fixed nn, continue, lower learning rate from step 846 and 1320 epoch',
-               resume='94688drl')
+               settings=wandb.Settings(start_method="thread"), notes='',
+               resume='jmwu3pmx', tags=['nn=[256,128,64,32]', f'lr={lr}', env_name, 'BC', f'use_state={use_state}'])
 
-    run_bc(bs, lr, n_epochs, num_traj, use_color, n_eval)
+    run_bc(env_name, env_prefix, bs, lr, n_epochs, num_traj, use_color,use_state, n_eval)
     wandb.finish()
 
 
